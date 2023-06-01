ff54e5c7a5aed2454681d9d68d9475b82e5aa6fb
Ivan <kosarevsky2000@yandex.ru>
2023-06-01 21:48:03 +0300

    initial commit

---
+++ HomeWorkBasic-7-1/HomeWorkBasic-7-1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+#define MODE 1
+#ifndef MODE
+#error Constant MODE is not defined
+#else
+#if (MODE == 1)
+void add() {
+    std::cout << "Работаю в боевом режиме\n";
+    double first{}, second{};
+    std::cout << "Введите первое число: ";
+    std::cin >> first;
+    std::cout << "Введите второе число: ";
+    std::cin >> second;
+    std::cout << "Результат сложения: " << first + second;
+}
+#elif (MODE == 0)
+void Test() {
+    std::cout << "Работаю в тестовом режиме";
+}
+#else 
+void UnknownMode() {
+    std::cout << "Неизвестный режим. Завершение работы";
+}
+#endif
+#endif // !MODE
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    add();
+}
